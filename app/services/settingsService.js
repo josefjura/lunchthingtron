089d2a6fcd3c8fa5e5d2edf4c0d5046c35f1ef9a
jurajosef <josef.jura@hotmail.com>
2015-11-13 14:51:35 +0100

    Added settingsService

---
+++ app/services/settingsService.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app')
+        .value('settings', {
+            restaurants: []
+        });
+
+    angular.module('app')
+        .service('settingsService', ['settings', SettingsService]);
+
+    function SettingsService(settings) {
+        return {
+            loadConfig: loadConfig,
+            saveConfig: saveConfig,
+            getConfig: getConfig
+        };
+    }
+
+
+})();
