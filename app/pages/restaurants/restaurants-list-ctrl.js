e19a19a74ce9a16e440fb28f886aeb18249e0ee2
jurajosef <josef.jura@hotmail.com>
2015-12-03 11:19:10 +0100

    Fixed restaurants list controller

--- app/pages/restaurants/restaurants-list-ctrl.js
+++ app/pages/restaurants/restaurants-list-ctrl.js
@@ -1,13 +1,14 @@
 'use strict';
 angular.module('app')
-  .controller('RestaurantsListCtrl', function ($log, $scope, Config, UserConfig) {
+  .controller('RestaurantsListCtrl', function ($log, $scope, UserConfig) {
     var self = this;    
     $log.log('Hello from your Controller: RestaurantsCtrl in module main:. This is your controller:', this);
-
-    this.refresh();
 
     this.refresh = function () {
       self.restaurants = UserConfig.getRestaurantList();
     }
 
+
+    this.refresh();
+    
   });
