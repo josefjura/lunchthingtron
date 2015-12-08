33422d4b441ea71503fa7844a7502ef16a3c3d8c
jurajosef <josef.jura@hotmail.com>
2015-12-08 16:52:34 +0100

    Moved tsd.json, added angular-material dts

--- app/typings/tsd.d.ts
+++ app/typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angular-material/angular-material.d.ts" />
