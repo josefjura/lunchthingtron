941543adb6beedaaca4f849aeb14773c068e1234
jurajosef <josef.jura@hotmail.com>
2015-12-08 16:55:21 +0100

    Removing typescript definitions from git

--- app/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="node/node.d.ts" />
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="angular-material/angular-material.d.ts" />
